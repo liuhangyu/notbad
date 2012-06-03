ff3b4df87abc42151c65a5ba8aef4c658fa749a9
myx <myx@LOOL.localdomain>
2012-06-03 16:13:17 +0400

    Use a static assertion to test the value of a constant expression

---
+++ DCL/dcl03.c
@@ -0,0 +1,36 @@
+/* Use a static assertion to test the value of a constant expression */
+
+/* BAD */
+
+struct timer {
+  uint8_t MODE;
+  uint32_t DATA;
+  uint32_t COUNT;
+};
+ 
+int func(void) {
+  assert(offsetof(timer, DATA) == 4);
+}
+
+/* NOT BAD */
+
+struct timer {
+  uint8_t MODE;
+  uint32_t DATA;
+  uint32_t COUNT;
+};
+ 
+#if (offsetof(timer, DATA) != 4)
+  #error "DATA must be at offset 4"
+#endif
+
+/* ALREADY NOT BAD */
+
+struct timer {
+  uint8_t MODE;
+  uint32_t DATA;
+  uint32_t COUNT;
+};
+ 
+// static_assert(constant-expression, string-literal); added in C11 and C++0x
+static_assert(offsetof(struct timer, DATA) == 4, "DATA must be at offset 4");
