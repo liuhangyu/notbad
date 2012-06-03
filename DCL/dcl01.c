1a1ac39ad11469dac5502fd624db490f93ab1111
myx <myx@LOOL.localdomain>
2012-06-03 15:59:46 +0400

    Do not reuse variable names in subscopes

--- DCL/dcl01.c
+++ DCL/dcl01.c
@@ -1,19 +1,46 @@
-/* Const-qualify immutable objects */
+/* Do not reuse variable names in subscopes */
 
 /* BAD */
 
-float pi = 3.14159f;
-float degrees;
-float radians;
-/*
-radians = degrees * pi / 180;
- */
+static char msg[100];
+static const size_t msgsize = sizeof( msg);
+ 
+void report_error(const char *str) {
+  char msg[80];
+  snprintf(msg, msgsize, "Error: %s\n", str);
+  /* ... */
+}
+ 
+int main() {
+  /* ... */
+  report_error("some error");
+}
 
 /* NOT BAD */
 
-const float pi = 3.14159f;
-float degrees;
-float radians;
-/*
-radians = degrees * pi / 180;
+static char message[100];
+static const size_t message_size = sizeof( message);
+ 
+void report_error(const char *str) {
+  char msg[80];
+  snprintf(msg, sizeof( msg), "Error: %s\n", str);
+  /* ... */
+}
+ 
+int main() {
+  /* ... */
+  report_error("some error");
+}
+
+// === EXCEPTION ==============================================================
+
+/* No problem, if the variable name used in the list of formal argument in the
+ * function declaration.
  */
+
+extern int name;
+void f(char *name);   // declaration: no problem here
+// ...
+void f(char *arg) {   // definition: no problem, arg doesn't hide name
+  // use arg
+}
